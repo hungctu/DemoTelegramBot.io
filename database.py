ad02b277e1099517ca4b38fa804698d6100d59d5
The Hung Nguyen <100697479+hungctu@users.noreply.github.com>
2023-08-04 22:33:43 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from aiogram.types import LabeledPrice
 
 PRICE = {
-    '1': [LabeledPrice(label='Charizard', amount=2000)],
-    '2': [LabeledPrice(label='Garchomp', amount=1500)],
-    '3': [LabeledPrice(label='Gengar', amount=1000)],
-    '4': [LabeledPrice(label='Gradevoir', amount=1300)],
-    '5': [LabeledPrice(label='Lucario', amount=1200)],
-    '6': [LabeledPrice(label='Mewtwo', amount=1800)]
+    '1': [LabeledPrice(label='Donut', amount=2000)],
+    '2': [LabeledPrice(label='Fries', amount=1500)],
+    '3': [LabeledPrice(label='Hambuger', amount=1000)],
+    '4': [LabeledPrice(label='Hotdot', amount=1300)],
+    '5': [LabeledPrice(label='Ice Cream', amount=1200)],
+    '6': [LabeledPrice(label='Pizza', amount=1800)]
 
 }
